42d0a296b4d50580f9fc55ca7e6c3b428e06d1e6
XtremeDevX <xtremedevx@gmail.com>
2021-02-27 22:19:15 +0530

    Update

---
+++ automate-package-list-update.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+os.chdir(r'C:\Users\xtrem\Desktop\Electric\electric-packages')
+packages = [ f.replace('.json', '') for f in os.listdir(r'C:\Users\xtrem\Desktop\Electric\electric-packages\packages') ]
+
+data = {
+    'packages': packages,
+}
+
+with open('package-list.json', 'w+') as f:
+    f.write(json.dumps(data, indent=4))
+os.system('powershell.exe deploy "Update Package List"')
+
+# RUN PACKAGE TESTS
+# for pkg in packages:
+#     ec1 = os.system(f'electric install {pkg}')
+#     ec2 = os.system(f'electric uninstall {pkg}')
+#     if ec1 and ec2 == 0:
+#         print(f'Passed Test : {pkg} Installation And Uninstallation')
