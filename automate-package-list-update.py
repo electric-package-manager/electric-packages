4be57fc06dd4ac180301378387c63eaf17ce8f44
XtremeDevX <xtremedevx@gmail.com>
2021-03-16 09:32:28 +0530

    Update

--- automate-package-list-update.py
+++ automate-package-list-update.py
@@ -1,16 +1,16 @@
 import json
 import os
 
-os.chdir(r'C:\Users\xtrem\Desktop\electric-packages')
-packages = [ f.replace('.json', '') for f in os.listdir(r'C:\Users\xtrem\Desktop\electric-packages\packages') ]
-
+os.chdir(r'C:\Users\xtrem\Desktop\electric\Electric Packages\packages')
+packages = [ f.replace('.json', '') for f in os.listdir(r'C:\Users\xtrem\Desktop\electric\Electric Packages\packages') ]
+print(packages)
 data = {
     'packages': packages,
 }
 
-with open('package-list.json', 'w+') as f:
+with open(r'C:\Users\xtrem\Desktop\electric\Electric Packages\package-list.json', 'w+') as f:
     f.write(json.dumps(data, indent=4))
-# os.system('powershell.exe deploy "Update Package List"')
+os.system('powershell.exe deploy "Update Package List"')
 
 # RUN PACKAGE TESTS
 # for pkg in packages:
