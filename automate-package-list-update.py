9e43a6717756b5fa5c687803ef2e3deec835c982
XtremeDevX <xtremedevx@gmail.com>
2021-03-17 15:38:08 +0530

    Removed Useless File

--- automate-package-list-update.py
+++ automate-package-list-update.py
@@ -11,10 +11,3 @@ data = {
 with open(r'C:\Users\xtrem\Desktop\electric\Electric Packages\package-list.json', 'w+') as f:
     f.write(json.dumps(data, indent=4))
 os.system('powershell.exe deploy "Update Package List"')
-
-# RUN PACKAGE TESTS
-# for pkg in packages:
-#     ec1 = os.system(f'electric install {pkg}')
-#     ec2 = os.system(f'electric uninstall {pkg}')
-#     if ec1 and ec2 == 0:
-#         print(f'Passed Test : {pkg} Installation And Uninstallation')
